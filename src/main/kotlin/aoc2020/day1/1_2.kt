29086598efd5653a14722919ede73fddb3a0e8e7
Nick Hauschild <nhauschild@phdata.io>
2020-12-02 19:46:08 -0600

    Update var names for 1.2

--- src/main/kotlin/aoc2020/day1/1_2.kt
+++ src/main/kotlin/aoc2020/day1/1_2.kt
@@ -5,10 +5,10 @@ import aoc2020.processedInput
 fun main() {
     val input = processedInput()
 
-    input.forEachIndexed { index, first ->
-        val midList = input.subList(index + 1, input.size)
-        midList.forEachIndexed { secondIndex, second ->
-            midList.subList(secondIndex + 1, midList.size).forEach { third ->
+    input.forEachIndexed { firstIdx, first ->
+        val inner = input.subList(firstIdx + 1, input.size)
+        inner.forEachIndexed { secondIdx, second ->
+            inner.subList(secondIdx + 1, inner.size).forEach { third ->
                 if(first + second + third == 2020) {
                     println("$first, $second, $third, ${first * second * third}")
                 }
