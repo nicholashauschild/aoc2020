f6e0e70654887e502f240d73fdf20db6f175f51b
Nick Hauschild <nhauschild@phdata.io>
2020-12-05 21:53:28 -0600

    Add tests for day2 puzzles

--- src/main/kotlin/aoc2020/day2/2_1.kt
+++ src/main/kotlin/aoc2020/day2/2_1.kt
@@ -1,13 +1,15 @@
 package aoc2020.day2
 
+internal val puzzleFunction1 = { pw: String, policyDetails: CorporatePasswordPolicyDetails ->
+    val min = policyDetails.firstInt
+    val max = policyDetails.secondInt
+    val letter = policyDetails.letter
+
+    pw.count { it == letter } in min..max
+}
+
 fun main() {
     val pwDatabase = processedInput()
 
-    println(pwDatabase.countValidEntries { pw, policyDetails ->
-        val min = policyDetails.firstInt
-        val max = policyDetails.secondInt
-        val letter = policyDetails.letter
-
-        pw.count { it == letter } in min..max
-    })
+    println(pwDatabase.countValidEntries(puzzleFunction1))
 }
--- src/main/kotlin/aoc2020/day2/2_2.kt
+++ src/main/kotlin/aoc2020/day2/2_2.kt
@@ -1,13 +1,15 @@
 package aoc2020.day2
 
+internal val puzzleFunction2 = { pw: String, policyDetails: CorporatePasswordPolicyDetails ->
+    val first = policyDetails.firstInt
+    val second = policyDetails.secondInt
+    val letter = policyDetails.letter
+
+    (pw[first - 1] == letter) xor (pw[second - 1] == letter)
+}
+
 fun main() {
     val pwDatabase = processedInput()
 
-    println(pwDatabase.countValidEntries { pw, policyDetails ->
-        val first = policyDetails.firstInt
-        val second = policyDetails.secondInt
-        val letter = policyDetails.letter
-
-        (pw[first - 1] == letter) xor (pw[second - 1] == letter)
-    })
+    println(pwDatabase.countValidEntries(puzzleFunction2))
 }
